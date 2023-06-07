a2d08c725e3c42fe2595212f3153fa913c60efff
adityasoni04 <aditya.soni020104@gmail.com>
2023-06-08 02:15:18 +0530

    React E-commerce App

---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,48 @@
+const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "Rs.2300",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "Rs.23,000",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "Rs.2,30,000",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "Rs.8,000",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "Rs.1600",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "Rs.18,000",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "Rs.24,000",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "Rs.2100",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "Rs.2500",
+  },
+]
+export default Ddata
